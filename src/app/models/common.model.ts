20a0f4493fa998754d22518cfbbda034c443cb9b
Taofeek Aderoju <taotara@yahoo.com>
2026-09-15 10:02:02 +0100

    Vehicle master page completed

---
+++ src/app/models/common.model.ts
@@ -0,0 +1,5 @@
+export interface IApiResponse {
+  message: string;
+  result: boolean;
+  data: any;
+}
--- src/app/pages/vehicle-master/vehicle-master.ts
+++ src/app/pages/vehicle-master/vehicle-master.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, OnInit, signal} from "@angular/core";
 import { Vehicle } from "../../models/vehicle.model";
 import { FormsModule } from "@angular/forms";
 import { HttpClient } from "@angular/common/http";
+import { IApiResponse } from "../../models/common.model";
 
 @Component({
   imports: [FormsModule],
@@ -9,18 +10,71 @@ import { HttpClient } from "@angular/common/http";
   styleUrl: "./vehicle-master.css",
   templateUrl: "./vehicle-master.html",
 })
-export class VehicleMaster {
+export class VehicleMaster implements OnInit {
 
   vehicleObj: Vehicle = new Vehicle();
+  vehicleList = signal<Vehicle[]>([])
 
   http = inject(HttpClient);
 
-  onSaveVehicle() {
-    this.http.post("https://freeapi.gerasim.in/api/CarRentalApp/CreateNewCar", this.vehicleObj).subscribe({
-      next:(res:any) => {
+  ngOnInit(): void {
+    this.getAllVehicles();
+  }
 
-      }, error:(err:any) => {
-        
+  getAllVehicles() {
+    this.http.get<IApiResponse>("https://freeapi.gerasim.in/api/CarRentalApp/GetCars").subscribe({
+      next:(res:IApiResponse) => {
+        this.vehicleList.set(res.data)
+      }
+    })
+  }
+
+  onEditRecord(data:Vehicle) {
+    this.vehicleObj = data;
+  }
+
+  onSaveVehicle() {
+    this.http.post<IApiResponse>("https://freeapi.gerasim.in/api/CarRentalApp/CreateNewCar", this.vehicleObj).subscribe({
+      next:(res:any) => {
+        if(res.result) {
+          alert("Vehicle added successfully")
+          this.getAllVehicles();
+        } else {
+          alert(res.message)
+        }
+      }, error:(err:IApiResponse) => {
+
+      }
+    })
+  }
+
+  onUpdateVehicle() {
+    this.http.put<IApiResponse>("https://freeapi.gerasim.in/api/CarRentalApp/updateCar", this.vehicleObj).subscribe({
+      next:(res:any) => {
+        if(res.result) {
+          alert("Vehicle updated successfully")
+          this.getAllVehicles();
+        } else {
+          alert(res.message)
+        }
+      }, error:(err:IApiResponse) => {
+
+      }
+    })
+  }
+
+  onDeleteVehicle(id: number) {
+    const isConfirmed = confirm("Are you sure you want to delete?")
+    this.http.delete<IApiResponse>("https://freeapi.gerasim.in/api/CarRentalApp/DeleteCarbyCarId" +id).subscribe({
+      next:(res:any) => {
+        if(res.result) {
+          alert("Vehicle deleted successfully")
+          this.getAllVehicles();
+        } else {
+          alert(res.message)
+        }
+      }, error:(err:IApiResponse) => {
+
       }
     })
   }
